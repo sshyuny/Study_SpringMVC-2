555dbfa43e14e1d836742af1266f48f55646d8a1
sshyuny <thgustls@gmail.com>
2023-01-22 23:13:16 +0900

    Form 전송 객체 분리 - 소개, 개발

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -11,18 +11,18 @@ import lombok.Data;
 @Data
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class) //수정 요구사항 추가
+    // @NotNull(groups = UpdateCheck.class) //수정 요구사항 추가
     private Long id;
 
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class}, message = "공백X")
+    // @NotBlank(groups = {SaveCheck.class, UpdateCheck.class}, message = "공백X")
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+    // @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    // @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = {SaveCheck.class})
+    // @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    // @Max(value = 9999, groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
